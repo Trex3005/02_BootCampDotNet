33ffac13674aeca3fd3e4d4c8132b2baa4ed8b63
Trex3005 <trexme.3005@gmail.com>
2024-10-24 12:59:52 +0700

    Push code session 3

---
+++ bt.session2/Program.cs
@@ -0,0 +1,102 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// #region Bài tập 1: Tính số ngày trong tuần và số ngày lẻ
+// // Input
+// // B1: Người dùng nhập số ngày
+// Console.WriteLine("Nhập số ngày:");
+// string? songay = Console.ReadLine();
+
+// // Output
+// // Số tuần và số ngày lẻ.
+// int sotuan = 0;
+// int songayle = 0;
+
+
+// // Process
+// // Tính toán số tuần và số ngày lẻ còn lại.
+// // Ví dụ: người dùng nhập 10 ngày thì số tuần là 1 và ngày lẻ là 3.
+// int formatsongay = Convert.ToInt32(songay);
+// sotuan = formatsongay / 7;
+// songayle = formatsongay % 7;
+// Console.WriteLine($"Số tuần là: {sotuan}");
+// Console.WriteLine($"Số ngày lẻ là: {songayle}");
+
+// #endregion
+
+// #region Bài tập 2: Tính tổng giá trị đơn hàng sau khi áp dụng giảm giá
+// // Input
+// // Nhập giá trị của đơn hàng là 1 ly trà sữa
+// Console.WriteLine("Nhập giá trị ly trà sữa: ");
+// string? sotienlytra = Console.ReadLine();
+// Console.WriteLine("Nhập phần trăm giảm giá: ");
+// string? phantramgiam = Console.ReadLine(); 
+
+// // Output
+// // Tổng giá tiền cần phải thanh toán
+// int sotiengiam = 0;
+// int tonggiatien = 0;
+
+// // Process
+// // Tính toán số tiền giảm giá và tổng số tiền phải thanh toán sau khi áp dụng giảm giá
+// int formatsotienlytra = Convert.ToInt32(sotienlytra);
+// int formatphantramgiam = Convert.ToInt32(phantramgiam);
+// sotiengiam = formatsotienlytra * formatphantramgiam / 100;
+// tonggiatien = formatsotienlytra - sotiengiam;
+// Console.WriteLine($"Tổng giá tiền ly trà sữa sau giảm giá là: {tonggiatien}");
+
+// #endregion
+
+#region Bài tập 3: Chuyển đổi thời gian từ phút sang giờ và phút
+// Input: Nhập vào tổng số phút
+Console.WriteLine("Mời bạn nhập tổng số phút: ");
+string? tongsophut = Console.ReadLine();
+
+// Output: Số giờ và số phút
+int sophut = 0;
+int sogio = 0;
+
+// Process: Chuyển đổi tổng số phút thành số giờ và số phút
+int formattongsophut = Convert.ToInt32(tongsophut);
+sogio = formattongsophut / 60;
+sophut = formattongsophut % 60;
+Console.WriteLine($"Số giờ là: {sogio} giờ");
+Console.WriteLine($"Số phút là: {sophut} phút");
+
+#endregion
+
+#region Bài tập 4: Tính tổng số tiền sau khi cộng thêm thuế VAT
+
+#endregion
+
+#region Bài tập 5: Chuyển đổi đơn vị tiền tệ
+
+#endregion
+
+#region Bài tập 1: Tính số ngày trong tuần và số ngày lẻ
+
+#endregion
+
+#region Bài tập 1: Tính số ngày trong tuần và số ngày lẻ
+
+#endregion
+
+#region Bài tập 1: Tính số ngày trong tuần và số ngày lẻ
+
+#endregion
+
+#region Bài tập 1: Tính số ngày trong tuần và số ngày lẻ
+
+#endregion
+
+#region Bài tập 1: Tính số ngày trong tuần và số ngày lẻ
+
+#endregion
+
+#region Bài tập 1: Tính số ngày trong tuần và số ngày lẻ
+
+#endregion
+
+#region Bài tập 1: Tính số ngày trong tuần và số ngày lẻ
+
+#endregion
