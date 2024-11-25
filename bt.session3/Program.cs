e3ac5a77c02c523ea052d44b40c393e61a12fa03
Trex3005 <trexme.3005@gmail.com>
2024-11-26 00:08:03 +0700

    push code

---
+++ bt.session3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #region Bài 1: Tính tổng các số trong mảng
+        // IStNumber = [20, 81, 97, 63, 72, 11, 20, 15, 33, 15, 41, 20]
+        // Bài toán: tính tổng các số trong mảng
+        // Mô tả: Bạn được cung cấp 1 mảng số nguyên IstNumber, nhiệm vụ của bạn là tính tổng các số trong mảng này.
+        // Input: IstNumber: Một danh sách (Mảng) các số nguyên, đây là mảng bạn cần tính tổng
+        // Output: Trả về tổng tất cả các số trong mảng IstNumber
+
+        // int [] IstNumber = {20, 81, 97, 63, 72, 11, 20, 15, 33, 15, 41, 20};
+        // int sum = 0;
+        
+        // foreach(int a in IstNumber)
+        // {
+        //     sum += a;
+        // }
+        // Console.WriteLine($"Tổng các số trong IstNumber là: {sum}" );
+
+        #endregion
+
+        #region Bài 2: Tìm hai số trong danh sách số nguyên sao cho tổng chúng bằng 1 giá trị cho trước
+        // Ist_number = [2, 7, 11, 15]
+        // Mô tả: Tìm hai số trong danh sách số nguyên sao cho tổng chúng bằng 1 giá trị cho trước
+        // Ví dụ: 
+        // Input: Nums = [2, 7, 11, 15, 3, 6], target = 9
+        // Output: [0, 1](vì nums[0] + nums[1] = 2 + 7 = 9) ngược lại nếu không có.
+
+        // int[] numbers1 = {2, 7, 11, 15, 3, 6};
+        // int targetSum = 9;
+        // bool found = false;
+
+        // for(int i = 0; i < numbers1.Length; i++)
+        // {
+        //     for(int j = i + 1; j < numbers1.Length; j++)
+        //     {
+        //         if(numbers1[i] + numbers1[j] == targetSum)
+        //         {
+        //             Console.WriteLine($"Hai số {numbers1[i]} và {numbers1[j]} có tổng là {targetSum}");
+        //             found = true;
+        //         }      
+        //     }
+        // }
+        // if (!found)
+        // {
+        //     Console.WriteLine($"Không có số nào có tổng bằng {targetSum}");
+        // }
+        
+        #endregion
+
+        #region
+
+        #endregion
+
+
+    }
+}
